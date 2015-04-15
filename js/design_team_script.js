55d1848ed7964908545aa8eb9eb4724a67000499
Lee <lee@MAC-LWILLIAMS.local>
2015-04-15 14:18:34 -0400

    made gh-pages

---
+++ js/design_team_script.js
@@ -0,0 +1,67 @@
+$( document ).ready(function() {
+	var play = false;
+
+	jQuery.extend( jQuery.easing,
+	{
+	    easeInOutQuad: function (x, t, b, c, d) {
+	        if ((t/=d/2) < 1) return c/2*t*t + b;
+	        return -c/2 * ((--t)*(t-2) - 1) + b;
+	    },
+	    easeInOut: function (x, t, b, c, d) {
+	        return jQuery.easing.easeInOutQuad(x, t, b, c, d);
+	    }
+	});
+
+
+	//for loading from local storage
+	if( sessionStorage.getItem("name") ) {
+		var name = JSON.parse(sessionStorage["name"]);
+		$('.first').html(name.first);
+		$('.last').html(name.last);
+	};
+
+	//for setting local storage
+	$('form .button').click( function() { 
+		var name = {
+			first: $('#fname').val(),
+			last: $('#lname').val()
+		};
+		$('.first').html(name.first);
+		$('.last').html(name.last);
+		sessionStorage["name"] = JSON.stringify(name);
+	});
+
+	// $('body').keyup(function(e){
+	// 	if (e.keyCode == 13 && !play ) {
+			drawSVGPaths($('#outlines'), 250, 50);
+			$('#outlines').fadeIn('slow');
+			play = true;
+	// 	};
+	// });
+
+
+	function drawSVGPaths(_parentElement, _duration, _timeDelay) {
+        var paths = $(_parentElement).find('path');
+        //for each PATH..
+        $.each( paths, function(i) {
+            //get the total length
+            var totalLength = this.getTotalLength();
+            //set PATHs to invisible
+            $(this).css({
+                'stroke-dashoffset': totalLength,
+                'stroke-dasharray': totalLength + ' ' + totalLength
+            });
+
+            $(this).delay(_timeDelay*i).animate({
+                'stroke-dashoffset': 0
+            }, {
+                duration: _duration,
+                easing: 'easeInOutQuad',
+                complete: function() {
+                   $(this).attr('fill', $(this).attr('stroke')).fadeIn('slow');
+                }
+            });
+        });
+    }
+
+});
